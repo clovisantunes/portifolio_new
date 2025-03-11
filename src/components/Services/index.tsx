8e47a1ee6cf5f2470e2c9307d9743f3a524ce40a
unknown <clovissantannaa@gmail.com>
2025-03-11 19:59:29 -0300

    fix

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,4 +1,4 @@
-import React, { JSX } from 'react';
+import { JSX } from 'react';
 import styles from "./styles.module.scss";
 import { FaCode, FaDatabase, FaSearch, FaServer } from 'react-icons/fa'; 
 
